03f2e6352365184ed0f5377f8ef723f33af41b0d
chaevsfe <pie@duck.com>
2026-09-10 14:12:00 -0400

    Nest the Create Fly Recipe Viewer inside the jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,12 @@ repositories {
     maven("https://api.modrinth.com/maven") {
         content { includeGroup("maven.modrinth") }
     }
+    flatDir {
+        dirs("libs", "../../create-rei/CreateReiViewer-Fly/build/libs")
+    }
 }
+
+val recipeViewer = ":CreateReiViewer:${property("createreiviewer_version")}+fabric-mc${property("minecraft_version")}"
 
 loom {
     mods {
@@ -36,6 +41,8 @@ dependencies {
     implementation("maven.modrinth:create-fly:${property("create_fabric_version")}")
 
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
+
+    include(recipeViewer)
 }
 
 java {
